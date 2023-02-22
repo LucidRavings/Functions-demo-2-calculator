58a8eb2ab3ee4a672594a5e83b9b2c859da0465d
LucidRavings <cybernerd82@yahoo.com>
2023-02-22 12:27:41 -0700

    initial commit

---
+++ starter.js
@@ -0,0 +1,98 @@
+////////////////////////
+////// CALCULATOR //////
+////////////////////////
+
+// CODE HERE
+
+// Each math operation is the callback function: add, subtract, multiply, devide.
+
+// inline arrow function with implicit return
+// Add function
+const add = (num1, num2) => num1 + num2
+
+// regular arrow function
+// Subtract function
+const subtract = (num1, num2) => {
+  return num1 - num2
+}
+
+// function expression
+// Multiply function
+const multiply = function (num1, num2){
+  return num1 * num2
+}
+
+//function declaration
+// Divide function
+function devide(num1, num2){
+  return num1 / num2
+}
+
+console.log(add(6, 4))
+
+///////////////////////
+////// PET STORE //////
+///////////////////////
+
+const dogProducts = [
+    {
+      name: 'leash',
+      colors: ['red', 'blue', 'green'],
+      category: 1,
+      inventory: 30,
+      basePrice: 13.99, 
+      displayPrice: 13.99
+    }, 
+    {
+      name: 'chew toy',
+      colors: ['brown'],
+      category: 2,
+      inventory: 120,
+      basePrice: 6.00, 
+      displayPrice: 6.00
+    }, 
+    {
+      name: 'rope',
+      colors: ['blue & green', 'red & yellow'],
+      category: 2,
+      inventory: 75,
+      basePrice: 4.99, 
+      displayPrice: 4.99
+    }
+]
+
+const catProducts = [
+  {
+    name: 'mouse toy', 
+    colors: ['pink', 'grey', 'black'], 
+    category: 2, 
+    inventory: 125, 
+    basePrice: 2.50, 
+    displayPrice: 2.50
+  },
+  {
+    name: 'cat sweater',
+    colors: ['black'],
+    category: 1,
+    inventory: 15,
+    basePrice: 10.00, 
+    displayPrice: 10.00
+  }, 
+  {
+    name: 'straching post',
+    colors: ['tan'],
+    category: 2,
+    inventory: 40,
+    basePrice: 22.99, 
+    displayPrice: 22.99
+  }
+]
+
+// CODE HERE
+
+
+////////////////////////
+////// SANDWICHES //////
+////////////////////////
+
+// CODE HERE
